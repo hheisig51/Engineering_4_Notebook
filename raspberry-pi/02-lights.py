80facd7e1ba2b71b05023858ff9f6964b076795f
hheisig51 <hheisig51@charlottesvilleschools.org>
2022-09-09 10:45:33 -0400

    Code

---
+++ raspberry-pi/02-lights.py
@@ -0,0 +1,6 @@
+# 2022-09-09, Em Heisig (hheisig51)
+# As the countdown (from 01-countdown.py) ticks down, a red light will blink every second. At 0 seconds (liftoff), a green LED will turn on.
+
+import board
+import time
+
