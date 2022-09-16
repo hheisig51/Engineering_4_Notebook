12dc55e0e1de0e4203afb1d78a750c7efe1ea0e8
hheisig51 <hheisig51@charlottesvilleschools.org>
2022-09-16 10:11:58 -0400

    Un-spicied 03-button.py

--- raspberry-pi/03-button.py
+++ raspberry-pi/03-button.py
@@ -13,9 +13,16 @@ r_led.direction = digitalio.Direction.OUTPUT
 button = digitalio.DigitalInOut(board.GP16)
 button.pull = digitalio.Pull.DOWN
 
-abort = 0
-
 while True:
     if button.value == True:
-        if abort == 0:
-            break
+        for x in range(10, 0, -1): # Moves from 10 to 0 in increments of 1
+            if x > 0:
+                print(f"{x} seconds left!") # Prints the number of seconds left
+                r_led.value = True # Turns the red LED on
+                time.sleep(.3)
+                r_led.value = False # Turns red LED off 0.3 seconds into the 1.0 second cycle.
+                time.sleep(.7)
+        print(f"Liftoff!") # Since x is not greater than 0, this prints
+        g_led.value = True
+        time.sleep(2)
+        g_led.value = False
